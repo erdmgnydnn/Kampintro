4eb4b2261efddd08a84d22a81b1f79205ea8518c
erdmgnydnn <erdembakigunaydn28@gmail.com>
2021-12-08 06:04:10 +0300

    Add project files.

---
+++ Kampintro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisimi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+
+             else
+                    {
+                        Console.WriteLine("degimedi butonu");
+                    }
+
+               
+            if (sistemeGirisYapmisimi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+              
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
---
+++ Work/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ProductName = "Buzdolabı";
+            product1.ProductPrice = 8000;
+            product1.Aproved = true;
+
+
+            Product product2 = new Product();
+            product2.ProductName = "Çamaşır Makinası";
+            product2.ProductPrice = 6000;
+            product2. Aproved = false;
+
+            Product[] products = new Product[] { product1, product2, };
+
+            Console.WriteLine("For Döngüsü");
+
+            for (int i = 0; i < products.Length; i++)
+            {
+
+                Console.WriteLine(products[i].ProductName + "adlı ürün fiyatı:" + products[i].ProductPrice +"adlı ürün fiyatı:" + products[i].Aproved +"onaylı durumu:" );
+            }
+
+            Console.WriteLine("While Döngüsü");
+
+            int x = 0;
+            while (x<products.Length)
+            {
+                Console.WriteLine(products[x].ProductName + "adlı ürün fiyatı:" + products[x].ProductPrice +   products[x].Aproved + "onaylı durumu:");
+                x++;
+            }
+            Console.WriteLine("Foreach Döngüsü");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.ProductName + "adlı ürün fiyatı:" + product.ProductPrice + "adlı ürün fiyatı:"+ product.Aproved + "onaylı durumu:");
+            }
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+
+        public bool Aproved { get; set; }
+
+
+    }
+
+}
